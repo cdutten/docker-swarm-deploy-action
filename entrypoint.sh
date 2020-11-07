2b50b0e6840a8b67d326dd6579389594904e796f
Cesar Dutten <cdutten@gmail.com>
2020-11-07 20:58:48 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,6 +28,7 @@ if [ ${INPUT_REMOTE_HOST#"ssh://"} != "$INPUT_REMOTE_HOST" ]; then
     printf '%s' "$INPUT_SSH_PRIVATE_KEY" > "$HOME/.ssh/docker"
     chmod 600 "$HOME/.ssh/docker"
     eval $(ssh-agent)
+    ls -la $HOME/.ssh/
     ssh-add "$HOME/.ssh/docker"
 
     # Add public key to known hosts.
